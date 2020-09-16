7862981fe4388fde5af7251c6c2e95b343cc51c9
Jaewon Chung <jaywonchung@snu.ac.kr>
2020-09-16 23:39:23 +0900

    Display all GPUs

--- scripts/gpu_usage.sh
+++ scripts/gpu_usage.sh
@@ -4,7 +4,7 @@ get_platform()
 {
 	case $(uname -s) in
 		Linux)
-			gpu=$(lspci -v | grep -i gpu | grep driver | awk '{print $5}')
+			gpu=$(lspci -v | grep VGA | head -n 1 | awk '{print $5}')
 			echo $gpu
 		;;
 
@@ -20,18 +20,18 @@ get_platform()
 get_gpu()
 {
 	gpu=$(get_platform)
-	if [ $gpu == nvidia-gpu ]; then
-		usage=$(nvidia-smi | grep "%" | awk '{print $13}')
-		echo $usage
+	if [[ "$gpu" == NVIDIA ]]; then
+    usage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{if ($1 < 10) {print "0"$1"%"} else {print $1"%"}}')
+  else
+    usage='unknown'
 	fi
+  echo $usage
 }
 main()
 {
 	gpu_usage=$(get_gpu)
 	echo "GPU $gpu_usage"
-	sleep 10
+	sleep 3
 }
 # run the main driver
 main
-
-
